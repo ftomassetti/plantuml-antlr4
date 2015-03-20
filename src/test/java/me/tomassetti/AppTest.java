d0076126aac0bc9a2f91dbaae0530828f5b04910
Federico Tomassetti <ftomassetti@groupon.com>
2015-03-20 12:46:36 +0000

    Implementing initial tests

--- src/test/java/me/tomassetti/AppTest.java
+++ src/test/java/me/tomassetti/AppTest.java
@@ -11,7 +11,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.List;
 
 /**
  * Unit test for simple App.
@@ -37,52 +39,66 @@ public class AppTest
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    /*public void testApp() throws IOException {
+    class ErrorCollector implements ANTLRErrorListener {
+        private List<String> errors = new ArrayList<>();
+        
+        public List<String> getErrors() {
+            return errors;
+        }
+        
+        @Override
+        public void syntaxError(Recognizer<?, ?> recognizer, Object o, int i, int i1, String s, RecognitionException e) {
+            errors.add(s);
+        }
+    
+        @Override
+        public void reportAmbiguity(Parser parser, DFA dfa, int i, int i1, boolean b, BitSet bitSet, ATNConfigSet atnConfigSet) {
+            errors.add("ambiguity");
+        }
+    
+        @Override
+        public void reportAttemptingFullContext(Parser parser, DFA dfa, int i, int i1, BitSet bitSet, ATNConfigSet atnConfigSet) {
+            errors.add("full context");
+        }
+    
+        @Override
+        public void reportContextSensitivity(Parser parser, DFA dfa, int i, int i1, int i2, ATNConfigSet atnConfigSet) {
+            errors.add("context sensitivity");
+        }
+    }
+    
+    public void testParser() throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("usecase1.plantuml").getFile());
         
+        ErrorCollector lexErrors = new ErrorCollector();
+        ErrorCollector syntaxErrors = new ErrorCollector();
+        
         PlantUMLLexer lexer = new PlantUMLLexer(new ANTLRInputStream(new FileInputStream(file)));
+        lexer.addErrorListener(lexErrors);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         PlantUMLParser parser = new PlantUMLParser(tokens);
+        parser.addErrorListener(syntaxErrors);
         PlantUMLParser.DiagramContext diagramCtx = parser.diagram();
-        assertTrue(true);
-    }*/
+        
+        assertEquals(0, lexErrors.getErrors().size());
+        assertEquals(0, syntaxErrors.getErrors().size());
+    }
 
     public void testLexer() throws IOException {
-        System.out.println("TEST LEXER");
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("usecase1.plantuml").getFile());
 
+        ErrorCollector lexErrors = new ErrorCollector();
+
         PlantUMLLexer lexer = new PlantUMLLexer(new ANTLRInputStream(new FileInputStream(file)));
-        lexer.addErrorListener(new ANTLRErrorListener() {
-            @Override
-            public void syntaxError(Recognizer<?, ?> recognizer, Object o, int i, int i1, String s, RecognitionException e) {
-                System.out.println("[ERROR] "+s);
-            }
-
-            @Override
-            public void reportAmbiguity(Parser parser, DFA dfa, int i, int i1, boolean b, BitSet bitSet, ATNConfigSet atnConfigSet) {
-                System.out.println("B");
-            }
-
-            @Override
-            public void reportAttemptingFullContext(Parser parser, DFA dfa, int i, int i1, BitSet bitSet, ATNConfigSet atnConfigSet) {
-                System.out.println("C");
-            }
-
-            @Override
-            public void reportContextSensitivity(Parser parser, DFA dfa, int i, int i1, int i2, ATNConfigSet atnConfigSet) {
-                System.out.println("D");
-            }
-        });
+        lexer.addErrorListener(lexErrors);
         for (Token token = lexer.nextToken();
              token.getType() != Token.EOF;
              token = lexer.nextToken())
         {
             System.out.println("* "+token);
         }
+        assertEquals(0, lexErrors.getErrors().size());
     }
 }
